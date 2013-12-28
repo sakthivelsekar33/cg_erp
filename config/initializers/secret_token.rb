c604f46f21ca5dd7b10884d0431b3aee981cd77d
Sakthivel S <sakthivelsekar33@gmail.com>
2013-12-28 13:03:14 +0530

    Commit 1 - 28_12_2013

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CgErpTwo::Application.config.secret_key_base = '660d2db2f366d8a6ab9ed0cbdb0a1a2eecf7d997c694d7361b5407ca51be6f88fd8121a9cd61d10447de2261d47d6a7c39dd0bc3f0e59a65ba3b2ee7b9f06d24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CgErpTwo::Application.config.session_store :cookie_store, key: '_cg_erp_two_session'
